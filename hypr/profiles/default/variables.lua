c5485e2d393a2ff8af6f6f30b872d525321f6603
Hieu Le Trung <warmdevofficial@gmail.com>
2026-06-13 19:25:57 +0700

    refactor(profile): setup profile and extract bind variables

---
+++ hypr/profiles/default/variables.lua
@@ -0,0 +1,18 @@
+return {
+    mainMod = "SUPER",
+    altMod = "ALT",
+
+    terminal = "kitty",
+    fileManager = "thunar",
+    browser = "brave --password-store=basic",
+    menu = "rofi -show drun",
+
+    waybarToggle = "~/.config/waybar/script/toggle.sh",
+    wallpaperSelector = "~/.config/hypr/script/wallpaper_selector.sh",
+    lockscreen = "~/.config/hypr/script/lockscreen.sh",
+    clipboard = "~/.config/hypr/script/clipboard.sh | wl-copy",
+
+    animationsEnabled = false,
+    waybarStart = "$HOME/.config/hypr/script/waybar.sh",
+    wallpaperStart = "awww-daemon\nsleep 1 && awww img $HOME/Pictures/wallpapers/MaiPhuongDangIu.jpg --transition-type center",
+}
