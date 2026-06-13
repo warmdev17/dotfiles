e2751f949cc307c361e24e81cba2211fd1b51cf2
Hieu Le Trung <warmdevofficial@gmail.com>
2026-06-13 19:27:10 +0700

    chore(profile): add minimal profile variables

---
+++ hypr/profiles/minimal/variables.lua
@@ -0,0 +1,19 @@
+return {
+    mainMod = "SUPER",
+    altMod = "ALT",
+
+    terminal = "kitty",
+    fileManager = "thunar",
+    browser = "brave",
+    menu = "rofi -show run",
+
+    waybarToggle = "pkill waybar || waybar",
+    wallpaperSelector = "",
+    lockscreen = "swaylock",
+    clipboard = "wl-copy",
+
+    animationsEnabled = false,
+    waybarStart = "waybar",
+    wallpaperDaemon = "swaybg",
+    wallpaperCmd = "swaybg -c '#000000'",
+}
