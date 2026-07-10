ebc8369637886d84f675f2e52972bb912aa2b863
Hieu Le Trung <warmdevofficial@gmail.com>
2026-07-10 13:06:15 +0700

    refactor: remove redundant three-finger swipe gestures from gesture configuration

--- hypr/base/gestures.lua
+++ hypr/base/gestures.lua
@@ -1,18 +1,6 @@
 return {
 	{ fingers = 3, direction = "horizontal", action = "workspace" },
 	{
-		fingers = 3,
-		direction = "up",
-		action = "fullscreen",
-	},
-	{
-		fingers = 3,
-		direction = "down",
-		action = "float",
-	},
-
-
-	{
 		fingers = 2,
 		direction = "pinch",
 		action = "cursorZoom",
