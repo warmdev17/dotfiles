77147060c8a6f6d76632b85756d203e23b7d7224
Hieu Le Trung <warmdevofficial@gmail.com>
2026-07-10 12:39:31 +0700

    feat: add more hyprland gestures

--- hypr/base/autostart.lua
+++ hypr/base/autostart.lua
@@ -1,10 +1,10 @@
 return {
-	fcitx_gtk = "export GTK_IM_MODULE=fcitx5",
-	fcitx_qt1 = "export QT_IM_MODULE=fcitx5",
-	fcitx_qt2 = "export QT_IM_MODULE=fcitx5",
-	fcitx_xmod = "export XMODIFIERS=@im=fcitx5",
-	fcitx_input = "export INPUT_METHOD=fcitx5",
-	fcitx_sdl = "export SDL_IM_MODULE=fcitx5",
+	fcitx_gtk = "set -Ux GTK_IM_MODULE=fcitx5",
+	fcitx_qt1 = "set -Ux QT_IM_MODULE=fcitx5",
+	fcitx_qt2 = "set -Ux QT_IM_MODULE=fcitx5",
+	fcitx_xmod = "set -Ux XMODIFIERS=@im=fcitx5",
+	fcitx_input = "set -Ux INPUT_METHOD=fcitx5",
+	fcitx_sdl = "set -Ux SDL_IM_MODULE=fcitx5",
 	fcitx_daemon = "fcitx5",
 
 	power_btn = "hyprctl keyword 'device[power-button]:enabled' false",
--- hypr/base/gestures.lua
+++ hypr/base/gestures.lua
@@ -1,3 +1,32 @@
 return {
 	{ fingers = 3, direction = "horizontal", action = "workspace" },
+	{
+		fingers = 3,
+		direction = "up",
+		action = "fullscreen",
+	},
+	{
+		fingers = 3,
+		direction = "down",
+		action = "float",
+	},
+	{
+		fingers = 3,
+		mods = "SUPER",
+		direction = "horizontal",
+		action = "move",
+	},
+	{
+		fingers = 3,
+		mods = "SUPER",
+		direction = "vertical",
+		action = "resize",
+	},
+	{
+		fingers = 2,
+		direction = "pinch",
+		action = "cursorZoom",
+		zoom_level = 1,
+		mode = "live",
+	},
 }
